96619cb63bcac4691de9d8f623712bb17c0cfdbe
Ken Sodemann <ken@ionic.io>
2023-01-09 12:50:55 -0600

    chore(): create project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.acconfigvue',
+  appName: 'AC Config Tool',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
