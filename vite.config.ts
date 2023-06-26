d5f7d482eb31f83263c1175748e423a4abccdea7
Ken Sodemann <ken@ionic.io>
2023-06-26 07:14:14 -0500

    chore(): update all dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,5 +19,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'tests/e2e'],
   },
 });
